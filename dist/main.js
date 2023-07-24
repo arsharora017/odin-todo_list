68e7e7c678dadf87c076f7972f31513c3c0e12e6
Arshdeep Arora <arsharora017@gmail.com>
2023-07-24 10:26:08 -0400

    add esLint and prettier to the project

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,7 @@
-(()=>{console.log("testing index.js webpack");const e=document.querySelector(".content"),t=document.createElement("h1");t.textContent="HELLO WORLD. ..TEST FROM WEBPACK INDEX. JS DOM MANIP",e.appendChild(t)})();
+(() => {
+  console.log("testing index.js webpack");
+  const e = document.querySelector(".content"),
+    t = document.createElement("h1");
+  (t.textContent = "HELLO WORLD. ..TEST FROM WEBPACK INDEX. JS DOM MANIP"),
+    e.appendChild(t);
+})();
